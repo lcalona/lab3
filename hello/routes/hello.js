24f17c3387250654d274ac2fd06e64d3317d641c
Luis Calona <calona.luis@gmail.com>
2020-01-23 10:02:59 -0800

    Updated hello app

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,10 @@
  */
 
 exports.view = function(req, res){
+  let nameToShow = req.params.userName || 'World';
+  console.log(`Name: ${nameToShow}`);
+
   res.render('index', {
-  	'name': '',
+  	'name': nameToShow,
   });
 };
