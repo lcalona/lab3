d6469dbcb211621fc8a3a565fbe27ff6299b9b8a
Luis Calona <calona.luis@gmail.com>
2020-01-23 09:50:05 -0800

    Added JS & CSS

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,26 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("The best way is just to observe the noise of the world.");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+/**
+ * Handler for "project" class element clicks.
+ */
+function projectClick(e) {
+	// Prevent the page from reloading
+	e.preventDefault();
+
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	if (description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else {
+		description.fadeToggle();
+	}
 }
